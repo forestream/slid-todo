32a85009c32844d7838165c90dc2d5772e9ffb02
MinaRoh <00minha@gmail.com>
2024-10-25 10:34:07 +0900

    ✨ #57 feat: 대시보드 목표 무한스크롤 기능 추가
    - 최초 렌더링시 3개, 하단 div ref에 닿을 때마다 3개씩 추가로 불러옵니다.

--- components/dashboard/GoalTodo.tsx
+++ components/dashboard/GoalTodo.tsx
@@ -1,9 +1,21 @@
+'use client';
 import { IconDashboardFlag } from '@/public/icons/IconDashboardFlag';
 import GoalTodoCard from './GoalTodoCard';
-import useGoalsQuery from '@/lib/hooks/useGoalsQuery';
+import useInfiniteGoalsQuery from '@/lib/hooks/useInfiniteGoalsQuery';
+import { useInView } from 'react-intersection-observer';
+import { useEffect } from 'react';
+import { Goal } from '@/lib/types';
 
 const GoalTodo = () => {
-  const { data: goals } = useGoalsQuery({ size: 3 }); // 3로 고정, 이후 무한스크롤로 3개씩.
+  const { ref, inView } = useInView({ threshold: 0.9 });
+  const { data, fetchNextPage } = useInfiniteGoalsQuery(3);
+
+  useEffect(() => {
+    if (inView) {
+      fetchNextPage();
+    }
+  }, [inView, fetchNextPage]);
+
   return (
     <section className='flex mt-6'>
       <div className='flex-col px-6 py-4 w-[344px] sm:w-[637px] lg:w-[1200px] h-[768px] bg-white rounded-xl border border-slate-100'>
@@ -14,14 +26,22 @@ const GoalTodo = () => {
           <p className='text-slate-800 text-lg font-semibold'>목표 별 할 일</p>
         </div>
         <div className='flex-col mt-6 space-y-4'>
-          {goals?.goals.map((goal) => (
-            <div key={goal.id} className='flex w-full h-[304px] p-6 bg-blue-50 rounded-[32px]'>
-              <GoalTodoCard goal={goal} />
+          {data?.pages.map((page, idx) => (
+            <div key={page.nextCursor || idx}>
+              {page.goals.map((goal: Goal) => (
+                <div key={goal.id} className='flex w-full h-[304px] p-6 bg-blue-50 rounded-[32px]'>
+                  <GoalTodoCard goal={goal} />
+                </div>
+              ))}
             </div>
           ))}
+          <div ref={ref} className='h-10'>
+            더보기
+          </div>
         </div>
       </div>
     </section>
   );
 };
+
 export default GoalTodo;
---
+++ lib/hooks/useInfiniteGoalsQuery.ts
@@ -0,0 +1,26 @@
+import { useInfiniteQuery } from '@tanstack/react-query';
+import { Goals } from '../types';
+import baseFetch from '../api/baseFetch';
+
+const useInfiniteGoalsQuery = (
+  size: number = 3,
+  sortOrder?: 'oldest' | 'newest',
+) => {
+  return useInfiniteQuery<Goals>({
+    queryKey: ['goals', { size, sortOrder }],
+    queryFn: ({ pageParam = null }) => {
+      const params = new URLSearchParams({
+        ...(pageParam !== null && { cursor: String(pageParam) }),
+        size: String(size),
+      });
+
+      return baseFetch(`/4-4-dev/goals?${params.toString()}`);
+    },
+    initialPageParam: null,  // 첫 요청에는 cursor가 null
+    getNextPageParam: (lastPage) => lastPage.nextCursor ?? null, // 다음 cursor 반환
+    refetchOnMount: false,
+    refetchOnWindowFocus: false,
+  });
+};
+
+export default useInfiniteGoalsQuery;
