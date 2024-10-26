36d29ffa0a3a37a50d97b72f9c6c57b86df7899b
mokoko <guswlso2@naver.com>
2024-10-26 12:18:36 +0900

    chore: test7

--- app/_view/QueryClientProvider.tsx
+++ app/_view/QueryClientProvider.tsx
@@ -1,10 +1,22 @@
 'use client';
 
 import { QueryClient, QueryClientProvider } from '@tanstack/react-query';
-import { PropsWithChildren } from 'react';
+import { PropsWithChildren, useState } from 'react';
 
 const QueryClientProviderWrapper = ({ children }: PropsWithChildren) => {
-  const queryClient = new QueryClient();
+  const [queryClient] = useState(
+    () =>
+      new QueryClient({
+        // 옵션 추가 가능
+        defaultOptions: {
+          queries: {
+            retry: 1,
+            retryDelay: 0,
+            staleTime: 60 * 1000,
+          },
+        },
+      })
+  );
 
   return <QueryClientProvider client={queryClient}>{children}</QueryClientProvider>;
 };
