fa47444fdeba09a822a909923d637571a485a8ec
hanbin <rj2303@naver.com>
2024-10-28 11:38:16 +0900

    🐛 #10 fix: 노트 생성 페이지 tiptap editor 컴포넌트 삽입

--- app/(nav)/todos/[todoId]/create/page.tsx
+++ app/(nav)/todos/[todoId]/create/page.tsx
@@ -1,11 +1,18 @@
+import TiptapEditorProvider from '@/components/TiptapEditorProvider';
+import LinkEmbed from '../_view/LinkEmbed';
 import NoteForm from '../_view/NoteForm';
 
 export default function Page() {
   return (
-    <main className='flex w-full'>
-      <section className='w-10 h-screen'></section> {/* 링크 임베드 */}
+    <main className='flex md:flex-col md:h-screen lg:flex-row w-full'>
+      <section className='lg:w-10 lg:h-screen'>
+        <LinkEmbed />
+      </section>
       <section className='max-w-[800px] w-full py-6 px-10 flex flex-col'>
-        <NoteForm />
+        <TiptapEditorProvider
+          className='resize-none w-full h-full focus-visible:outline-none text-slate-700 whitespace-break-spaces'
+          slotBefore={<NoteForm />}
+        />
       </section>
     </main>
   );
