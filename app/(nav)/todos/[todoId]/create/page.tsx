248b683cafb18c87085c6fcd5dc2fd65d537b27b
hanbin <rj2303@naver.com>
2024-10-28 12:55:45 +0900

    ✨ #10 feat: iframe으로 링크임베드 기능 개발
    - iframe을 허용하는 리스폰스만 보여줌

--- app/(nav)/todos/[todoId]/create/page.tsx
+++ app/(nav)/todos/[todoId]/create/page.tsx
@@ -1,19 +1,13 @@
 import TiptapEditorProvider from '@/components/TiptapEditorProvider';
-import LinkEmbed from '../_view/LinkEmbed';
 import NoteForm from '../_view/NoteForm';
 
 export default function Page() {
   return (
     <main className='flex md:flex-col md:h-screen lg:flex-row w-full'>
-      <section className='lg:w-10 lg:h-screen'>
-        <LinkEmbed />
-      </section>
-      <section className='max-w-[800px] w-full py-6 px-10 flex flex-col'>
-        <TiptapEditorProvider
-          className='resize-none w-full h-full focus-visible:outline-none text-slate-700 whitespace-break-spaces'
-          slotBefore={<NoteForm />}
-        />
-      </section>
+      <TiptapEditorProvider
+        className='resize-none w-full h-full focus-visible:outline-none text-slate-700 whitespace-break-spaces'
+        slotBefore={<NoteForm />}
+      />
     </main>
   );
 }
