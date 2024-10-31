4acad84bb74f92b035aa207af345c5b109fe07dc
MinaRoh <00minha@gmail.com>
2024-10-31 15:43:48 +0900

    💄 #128 style: 대시보드 할 일 버튼 모바일 디자인 수정
    - 불필요한 mr을 제거했습니다.

--- components/nav/NavDashBoard.tsx
+++ components/nav/NavDashBoard.tsx
@@ -14,7 +14,7 @@ const NavDashBoard = () => {
           <div className='text-lg font-medium text-slate-800'>대시보드</div>
         </div>
       </Link>
-      <div className='flex items-center sm:hidden lg:hidden mr-5'>
+      <div className='flex items-center sm:hidden lg:hidden'>
         <ButtonSlid className='gap-[2px] rounded-xl text-sm w-[84px] px-3 py-2'>
           <IconPlusSmall />
           <span>새 할 일</span>
