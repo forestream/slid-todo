5015c8d9fa7159f7079aba27b888ef1e1ff3c1d3
MinaRoh <00minha@gmail.com>
2024-10-31 15:36:24 +0900

    💄 #128 style: nav 목표 디자인 추가
    - hover, clicked 상태시 목표 아이템의 배경색을 변경했습니다.
    - 할 일 추가시 텍스트 앞의 기호 작동 방식을 변경했습니다.

--- components/nav/NavGoal.tsx
+++ components/nav/NavGoal.tsx
@@ -1,15 +1,22 @@
 'use client';
 import { IconFlagSmall } from '@/public/icons/IconFlagSmall';
 import useInfiniteGoalsQuery from '@/lib/hooks/useInfiniteGoalsQuery';
-import { SetStateAction, useEffect, useState } from 'react';
+import { useEffect, useState } from 'react';
 import { useInView } from 'react-intersection-observer';
 import { Goal } from '@/lib/types';
 import Link from 'next/link';
 import { useAddGoalMutation } from '@/lib/hooks/useAddGoalMutation';
 import InputSlid from '../common/InputSlid';
 import AddGoalButton from './AddGoalButton';
+import clsx from 'clsx';
 
 const NavGoal = () => {
+  const [isGoalInputVisible, setIsGoalInputVisible] = useState(false);
+  const [goalInputValue, setGoalInputValue] = useState('');
+  const [selectedGoalId, setSelectedGoalId] = useState<number | null>(null);
+
+  const addGoal = useAddGoalMutation();
+
   const { ref, inView } = useInView({ threshold: 0.9 });
   const { data, fetchNextPage } = useInfiniteGoalsQuery(20);
 
@@ -19,16 +26,24 @@ const NavGoal = () => {
     }
   }, [inView, fetchNextPage]);
 
-  const [isGoalInputVisible, setIsGoalInputVisible] = useState(false);
-  const [goalInputValue, setGoalInputValue] = useState('· ');
-  const addGoal = useAddGoalMutation();
+  const handleGoalClick = (goalId: number) => {
+    console.log(goalId);
+    setSelectedGoalId(goalId);
+  };
 
   const handleAddGoalButtonClick = () => {
     setIsGoalInputVisible(!isGoalInputVisible);
+    setGoalInputValue('· ');
   };
 
-  const handleInputChange = (e: { target: { value: SetStateAction<string> } }) => {
-    setGoalInputValue(e.target.value);
+  const handleInputChange = (e: React.ChangeEvent<HTMLInputElement>) => {
+    const inputValue = e.target.value;
+    // 유저가 `·` 기호를 지우지 못하게 항상 앞에 추가
+    if (!inputValue.startsWith('· ')) {
+      setGoalInputValue(`· ${inputValue.replace(/^·\s*/, '')}`);
+    } else {
+      setGoalInputValue(inputValue);
+    }
   };
 
   const handleGoalSubmit = (e: React.FormEvent) => {
@@ -49,7 +64,7 @@ const NavGoal = () => {
 
       {/* 새 목표 버튼 (모바일에서는 타이틀 옆, 태블릿과 데스크탑에서는 맨 아래로) */}
       <AddGoalButton
-        className='order-2 sm:order-4 lg:order-4 ml-auto sm:ml-0 lg:ml-0 gap-[2px] rounded-xl text-sm px-3 py-2 sm:px-6 lg:px-6 mt-0 sm:mt-4 lg:mt-4 w-[84px] sm:w-full lg:w-full'
+        className='order-2 sm:order-4 lg:order-4 ml-auto sm:ml-0 lg:ml-0 gap-[2px] rounded-xl text-sm px-3 py-2 sm:p-3 lg:p-3 sm:px-6 lg:px-6 mt-0 w-[84px] sm:w-full lg:w-full'
         onClick={handleAddGoalButtonClick}
       />
 
@@ -57,7 +72,15 @@ const NavGoal = () => {
         {data?.pages.map((page, idx) => (
           <div key={page.nextCursor || idx} className='flex-col'>
             {page.goals.map((goal: Goal) => (
-              <Link href={`/goals/${goal.id}`} key={goal.id} className='flex p-2 text-slate-700 text-sm font-medium'>
+              <Link
+                href={`/goals/${goal.id}`}
+                key={goal.id}
+                className={clsx(
+                  'flex text-slate-700 text-sm font-medium rounded-lg p-2 hover:bg-slate-50',
+                  goal.id === selectedGoalId ? 'bg-slate-50' : 'bg-white'
+                )}
+                onClick={() => handleGoalClick(goal.id)}
+              >
                 {`· ${goal.title}`}
               </Link>
             ))}
@@ -69,12 +92,13 @@ const NavGoal = () => {
       {/* 새 목표 클릭시 생성되는 인풋 */}
       {isGoalInputVisible && (
         <div className='order-4 sm:order-3 lg:order-3 w-full flex items-center mt-4'>
-          <form onSubmit={handleGoalSubmit} className='w-full'>
+          <form onSubmit={handleGoalSubmit} className='w-full h-auto'>
             <InputSlid
               type='text'
-              inputClassName='flex p-2 text-sm font-medium bg-white'
+              inputClassName='flex-grow p-2 text-sm font-medium bg-white rounded-lg'
               value={goalInputValue}
               onChange={handleInputChange}
+              autoFocus
             />
           </form>
         </div>
