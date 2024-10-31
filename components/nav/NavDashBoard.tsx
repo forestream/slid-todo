d6f9172c59cbec08bfa50686e302ebb38fe92fbd
MinaRoh <00minha@gmail.com>
2024-10-31 16:08:45 +0900

    🐛 #128 fix: 모바일에서도 새 할일 추가 가능하도록 수정
    - 모바일 뷰에서도 새 할일 버튼 클릭시 할 일 추가 모달이 뜨도록 수정했습니다.
    - 추후 버튼을 통합하여 리팩토링 될 수 있습니다.

--- components/nav/NavDashBoard.tsx
+++ components/nav/NavDashBoard.tsx
@@ -1,26 +1,37 @@
+'use client';
 import { IconDashboardSmall } from '@/public/icons/IconDashboardSmall';
 import ButtonSlid from '../common/ButtonSlid';
 import { IconPlusSmall } from '@/public/icons/IconPlusSmall';
 import Link from 'next/link';
+import { useRef } from 'react';
+import TodoAddModal from '../modal/todoModal/TodoAddModal';
 
 const NavDashBoard = () => {
+  const modalRef = useRef<HTMLButtonElement>(null);
+
+  const handleModalOpen = () => {
+    modalRef.current?.click();
+  };
   return (
-    <div className='w-full flex justify-between px-4 py-6 gap-2 text-nowrap'>
-      <Link href='/dashboard' className='flex gap-2'>
-        <div className='w-6 h-6 flex justify-center items-center'>
-          <IconDashboardSmall />
+    <>
+      <div className='w-full flex justify-between px-4 py-6 gap-2 text-nowrap'>
+        <Link href='/dashboard' className='flex gap-2'>
+          <div className='w-6 h-6 flex justify-center items-center'>
+            <IconDashboardSmall />
+          </div>
+          <div className='flex-grow flex-col'>
+            <div className='text-lg font-medium text-slate-800'>대시보드</div>
+          </div>
+        </Link>
+        <div className='flex items-center sm:hidden lg:hidden'>
+          <ButtonSlid onClick={handleModalOpen} className='gap-[2px] rounded-xl text-sm w-[84px] px-3 py-2'>
+            <IconPlusSmall />
+            <span>새 할 일</span>
+          </ButtonSlid>
         </div>
-        <div className='flex-grow flex-col'>
-          <div className='text-lg font-medium text-slate-800'>대시보드</div>
-        </div>
-      </Link>
-      <div className='flex items-center sm:hidden lg:hidden'>
-        <ButtonSlid className='gap-[2px] rounded-xl text-sm w-[84px] px-3 py-2'>
-          <IconPlusSmall />
-          <span>새 할 일</span>
-        </ButtonSlid>
       </div>
-    </div>
+      <TodoAddModal ref={modalRef} />
+    </>
   );
 };
 export default NavDashBoard;
