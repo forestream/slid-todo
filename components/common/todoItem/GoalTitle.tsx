977fcf8301258c8c117257fc5dfb2e35200904f4
mokoko <guswlso2@naver.com>
2024-11-01 10:56:15 +0900

    ✨ #157 feat: 목표타이틀을 클릭하면 해당 목표로 이동

--- components/common/todoItem/GoalTitle.tsx
+++ components/common/todoItem/GoalTitle.tsx
@@ -1,12 +1,15 @@
 import { GoalInTodo } from '@/lib/types/todo';
 import IconGoal from '@/public/icons/IconGoal';
+import Link from 'next/link';
 
 const GoalTitle = ({ goal }: { goal: GoalInTodo }) => {
   return (
-    <div className='flex gap-x-1.5 ml-8 cursor-pointer hover:text-link'>
-      <IconGoal />
-      <div className='truncate'>{goal.title}</div>
-    </div>
+    <Link href={`/goals/${goal.id}`}>
+      <div className='flex gap-x-1.5 ml-8 cursor-pointer hover:text-link'>
+        <IconGoal />
+        <div className='truncate'>{goal.title}</div>
+      </div>
+    </Link>
   );
 };
 
