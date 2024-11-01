1a26bae4e4bde56f852745caa289b0c276fb51fc
mokoko <guswlso2@naver.com>
2024-11-01 14:27:47 +0900

    💄 #169 style: TodoContentsDrawer 기본 스타일링 및 타이틀에 연결

--- components/common/todoItem/TodoTitle.tsx
+++ components/common/todoItem/TodoTitle.tsx
@@ -1,6 +1,8 @@
+import TodoContentsDrawer from '@/components/drawer/TodoContentsDrawer';
 import { MOBILE_BREAKPOINT } from '@/constants';
 import { Todo } from '@/lib/types/todo';
 import clsx from 'clsx';
+import { useState } from 'react';
 import { twMerge } from 'tailwind-merge';
 
 interface TodoTitleProps {
@@ -8,21 +10,26 @@ interface TodoTitleProps {
 }
 
 const TodoTitle: React.FC<TodoTitleProps> = ({ data }) => {
+  const [isOpen, setIsOpen] = useState(false);
   const handleTitleClick = () => {
     if (window.innerWidth >= MOBILE_BREAKPOINT) {
       //모바일이 아닐 때
     } else {
       // 모바일 일 때
+      setIsOpen(true);
     }
   };
 
   return (
-    <div
-      onClick={handleTitleClick}
-      className={twMerge(clsx('truncate hover:text-link cursor-pointer', data.done && 'line-through'))}
-    >
-      {data.title}
-    </div>
+    <>
+      <div
+        onClick={handleTitleClick}
+        className={twMerge(clsx('truncate hover:text-link cursor-pointer', data.done && 'line-through'))}
+      >
+        {data.title}
+      </div>
+      <TodoContentsDrawer isopen={isOpen} onChangeIsOpen={setIsOpen} data={data} />
+    </>
   );
 };
 
