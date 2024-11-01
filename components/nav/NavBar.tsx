39025a6d042a01b3d051aa0bf1ecce8b932a9b77
MinaRoh <00minha@gmail.com>
2024-11-01 15:12:22 +0900

    ✨ #31 feat: 태블릿에서 nav를 모달처럼 사용하는 기능 추가

--- components/nav/NavBar.tsx
+++ components/nav/NavBar.tsx
@@ -1,20 +1,25 @@
 'use client';
 import { ImageLogoWithText } from '@/public/images/ImageLogoWithText';
 import { IconFold } from '@/public/icons/IconFold';
-import Link from 'next/link';
+import { IconDashboardSmall } from '@/public/icons/IconDashboardSmall';
+import { ImageLogo } from '@/public/images/ImageLogo';
+import Button from '../common/ButtonSlid';
 import Profile from './NavProfile';
 import NavGoal from './NavGoal';
-import { useRef, useState } from 'react';
 import TodoAddModal from '../modal/todoModal/TodoAddModal';
 import NavAllTodos from './NavAllTodos';
 import AddTodoButton from './AddTodoButton';
-import { IconDashboardSmall } from '@/public/icons/IconDashboardSmall';
-import Button from '../common/ButtonSlid';
-import { ImageLogo } from '@/public/images/ImageLogo';
+import { MOBILE_BREAKPOINT, TABLET_BREAKPOINT } from '@/constants';
+import { SheetContent, SheetProvider, SheetTrigger } from '../common/Sheet';
+import { useRef, useState } from 'react';
+import Link from 'next/link';
 
 const NavBar = () => {
   const modalRef = useRef<HTMLButtonElement>(null);
-  const [isNavFolded, setIsNavFolded] = useState(false);
+  const [isNavOpened, setIsNavOpened] = useState(false);
+
+  // 태블릿에서만 nav를 좌측 modal처럼 사용
+  const isTablet = () => MOBILE_BREAKPOINT < window.innerWidth && window.innerWidth < TABLET_BREAKPOINT;
 
   const handleModalOpen = () => {
     modalRef.current?.click();
@@ -22,12 +27,69 @@ const NavBar = () => {
 
   const handleFoldButtonClick = () => {
     console.log('fold clicked');
-    setIsNavFolded(!isNavFolded);
+    setIsNavOpened(!isNavOpened);
   };
+
+  const NavContent = () => (
+    <div className='flex-shrink-0 w-screen h-screen flex-col sm:w-[280px] divide-slate-200 sm:border-r-[1px]'>
+      <nav className='flex-col w-full h-full'>
+        <div className='flex justify-between items-center p-4'>
+          <Link className='py-2 px-[5px]' href='/dashboard'>
+            <ImageLogoWithText />
+          </Link>
+          <Button
+            className='flex justify-center items-center sm:block w-6 h-6 p-1 bg-white hover:bg-slate-100 active:bg-slate-300 rounded-lg border-[1.5px] border-slate-400'
+            onClick={handleFoldButtonClick}
+          >
+            <IconFold isFold={false} />
+          </Button>
+        </div>
+        <div className='flex flex-col divide-y divide-slate-200'>
+          <Profile className='sm:border-none lg:border-none' />
+          {/* 대시보드 라벨과 새 할일 버튼 */}
+          <div className='flex flex-row sm:flex-col lg:flex-col divide-y divide-slate-200 sm:border-none lg:border-none'>
+            {/* 새 할일 버튼 */}
+            <div className='flex sm:justify-center items-center border-none px-4 py-6 order-2 sm:order-1 lg:order-1 ml-auto sm:w-full lg:w-full'>
+              <AddTodoButton
+                className='sm:mx-0 lg:mx-0 gap-[2px] rounded-xl text-sm px-3 py-2 sm:py-3 lg:py-3 sm:px-6 lg:px-6 mt-0 w-[84px] sm:w-full lg:w-full'
+                onClick={handleModalOpen}
+              />
+            </div>
+            {/* 대시보드 라벨 */}
+            <div className='px-4 py-6 order-1 sm:order-2 lg:order-2'>
+              <Link href='/dashboard' className='flex gap-2 w-full'>
+                <div className='w-6 h-6 flex justify-center items-center'>
+                  <IconDashboardSmall />
+                </div>
+                <div className='flex-grow flex-col'>
+                  <div className='text-lg font-medium text-slate-800'>대시보드</div>
+                </div>
+              </Link>
+            </div>
+          </div>
+          <NavAllTodos />
+          <NavGoal />
+        </div>
+      </nav>
+    </div>
+  );
 
   return (
     <>
-      {isNavFolded ? (
+      {isNavOpened ? (
+        isTablet() ? (
+          <SheetProvider isOpen={isNavOpened} onChangeIsOpen={setIsNavOpened}>
+            <SheetTrigger>
+              <IconFold isFold={false} />
+            </SheetTrigger>
+            <SheetContent position='left' className='w-[280px] p-0'>
+              <NavContent />
+            </SheetContent>
+          </SheetProvider>
+        ) : (
+          <NavContent />
+        )
+      ) : (
         <nav className='pt-4 px-[14px] flex flex-col gap-4 items-center sm:border-r-[1px]'>
           <Link href='/dashboard'>
             <ImageLogo />
@@ -39,48 +101,6 @@ const NavBar = () => {
             <IconFold isFold />
           </Button>
         </nav>
-      ) : (
-        <div className='flex-shrink-0 w-screen h-screen flex-col sm:w-[280px] divide-slate-200 sm:border-r-[1px]'>
-          <nav className='flex-col w-full h-full'>
-            <div className='flex justify-between items-center p-4'>
-              <Link className='py-2 px-[5px]' href='/dashboard'>
-                <ImageLogoWithText />
-              </Link>
-              <Button
-                className='flex justify-center items-center sm:block w-6 h-6 p-1 bg-white hover:bg-slate-100 active:bg-slate-300  rounded-lg border-[1.5px] border-slate-400'
-                onClick={handleFoldButtonClick}
-              >
-                <IconFold isFold={false} />
-              </Button>
-            </div>
-            <div className='flex flex-col divide-y divide-slate-200'>
-              <Profile className='sm:border-none lg:border-none' />
-              {/* 대시보드 라벨과 새 할일 버튼 */}
-              <div className='flex flex-row sm:flex-col lg:flex-col divide-y divide-slate-200 sm:border-none lg:border-none'>
-                {/* 새 할일 버튼 */}
-                <div className='flex sm:justify-center items-center border-none px-4 py-6 order-2 sm:order-1 lg:order-1 ml-auto sm:w-full lg:w-full'>
-                  <AddTodoButton
-                    className='sm:mx-0 lg:mx-0 gap-[2px] rounded-xl text-sm px-3 py-2 sm:py-3 lg:py-3 sm:px-6 lg:px-6 mt-0 w-[84px] sm:w-full lg:w-full'
-                    onClick={handleModalOpen}
-                  />
-                </div>
-                {/* 대시보드 라벨 */}
-                <div className='px-4 py-6 order-1 sm:order-2 lg:order-2'>
-                  <Link href='/dashboard' className='flex gap-2 w-full'>
-                    <div className='w-6 h-6 flex justify-center items-center'>
-                      <IconDashboardSmall />
-                    </div>
-                    <div className='flex-grow flex-col'>
-                      <div className='text-lg font-medium text-slate-800'>대시보드</div>
-                    </div>
-                  </Link>
-                </div>
-              </div>
-              <NavAllTodos />
-              <NavGoal />
-            </div>
-          </nav>
-        </div>
       )}
 
       <TodoAddModal ref={modalRef} />
--- constants/index.ts
+++ constants/index.ts
@@ -1,3 +1,4 @@
 export const API_BASE_URL = process.env.NEXT_PUBLIC_BASE_URL;
 
 export const MOBILE_BREAKPOINT = 640;
+export const TABLET_BREAKPOINT = 1024;
