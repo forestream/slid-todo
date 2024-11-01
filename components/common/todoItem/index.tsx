a2a90178a9227ccda97afb9042be617dd68ecaa7
mokoko <guswlso2@naver.com>
2024-11-01 10:53:03 +0900

    ✨ #157 feat: 타이틀 클릭 이벤트 모바일 구분

--- components/common/todoItem/index.tsx
+++ components/common/todoItem/index.tsx
@@ -1,11 +1,10 @@
 'use client';
 
-import { twMerge } from 'tailwind-merge';
-import clsx from 'clsx';
 import { Todo } from '@/lib/types/todo';
 import CheckIcon from './CheckIcon';
 import TodoIcon from './TodoIcon';
 import GoalTitle from './GoalTitle';
+import TodoTitle from './TodoTitle';
 
 interface TodoItemProps {
   data: Todo;
@@ -18,9 +17,7 @@ const TodoItem: React.FC<TodoItemProps> = ({ data, viewGoal }) => {
       <div className='flex justify-between items-center'>
         <div className='flex items-center gap-x-2 min-w-0 flex-1'>
           <CheckIcon done={data.done} id={data.id} />
-          <div className={twMerge(clsx('truncate hover:text-link cursor-pointer', data.done && 'line-through'))}>
-            {data.title}
-          </div>
+          <TodoTitle data={data} />
         </div>
         <div className='flex transition-all duration-200 group-hover:w-auto group-focus-within:w-auto'>
           <TodoIcon data={data} />
