9a0504b86bfa5e18ba7d976884743eec120b6b23
MinaRoh <00minha@gmail.com>
2024-11-05 17:26:59 +0900

    ✨ #216 feat: 대시보드 원형 프로그레스바 및 애니메이션 추가

--- components/dashboard/ProgressText.tsx
+++ components/dashboard/ProgressText.tsx
@@ -1,13 +1,9 @@
-import useTodoProgressQuery from '@/lib/hooks/useTodoProgressQuery';
-
-const ProgressText = () => {
-  const { data } = useTodoProgressQuery();
-  const percentage = data?.progress || 0;
+const ProgressText = ({ percentage }: { percentage: number }) => {
   return (
     <div className='flex-col mt-4'>
-      <p className='text-white text-lg font-semibold text-nowrap'>내 진행 상황</p>
+      <p className='text-white text-lg font-semibold'>내 진행 상황</p>
       <span className='flex mt-1 items-center gap-1 text-nowrap'>
-        <p className='text-white text-3xl font-bold'>{percentage}</p>
+        <p className='text-white text-3xl font-bold'>{percentage || 0}</p>
         <p className='text-white text-base font-semibold'>%</p>
       </span>
     </div>
