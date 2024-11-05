d117c3a52eed267e3e8ed1892114a65c55486b3b
mokoko <guswlso2@naver.com>
2024-11-05 13:20:38 +0900

    💄 #211 style: 아이콘 컴포넌트에 접근성 제목 추가 및 색상 변경

--- public/icons/IconNoteView.tsx
+++ public/icons/IconNoteView.tsx
@@ -6,27 +6,36 @@ interface IconNoteViewProps extends SVGProps<SVGSVGElement> {
 }
 
 const IconNoteView = ({ circleClassName, pathClassName, ...props }: IconNoteViewProps) => (
-  <svg width='24' height='24' viewBox='0 0 24 24' fill='none' xmlns='http://www.w3.org/2000/svg' {...props}>
-    <circle className={circleClassName} cx='12' cy='12' r='12' fill='#F8FAFC' />
-    <rect x='7.5' y='6.90039' width='9' height='10.3846' rx='1.38462' fill='#60A5FA' />
+  <svg
+    width='24'
+    height='24'
+    viewBox='0 0 24 24'
+    fill='none'
+    xmlns='http://www.w3.org/2000/svg'
+    aria-labelledby='noteViewTitle'
+    {...props}
+  >
+    <title id='noteViewTitle'>노트보기</title>
+    <circle className={circleClassName} cx='12' cy='12' r='12' fill='#FF9F43' />
+    <rect x='7.5' y='6.90039' width='9' height='10.3846' rx='1.38462' fill='#F8FAFC' />
     <path
       className={pathClassName}
       d='M9.57715 10.0156H14.4233'
-      stroke='white'
+      stroke='#FF9F43'
       strokeWidth='0.969231'
       strokeLinecap='round'
     />
     <path
       className={pathClassName}
       d='M9.57715 12.0918H14.4233'
-      stroke='white'
+      stroke='#FF9F43'
       strokeWidth='0.969231'
       strokeLinecap='round'
     />
     <path
       className={pathClassName}
       d='M9.57715 14.1699H14.4233'
-      stroke='white'
+      stroke='#FF9F43'
       strokeWidth='0.969231'
       strokeLinecap='round'
     />
