9aa446ece07955f976275f624806a65ff306b0f4
MinaRoh <00minha@gmail.com>
2024-11-07 16:59:54 +0900

    ✨  #242 feat: 대시보드 및 목표 상세 페이지 ProgressBar 애니메이션 0부터 실행되도록 수정

--- components/common/ProgressBar.tsx
+++ components/common/ProgressBar.tsx
@@ -1,8 +1,19 @@
+'use client';
 import clsx from 'clsx';
+import { useEffect, useState } from 'react';
 import { twMerge } from 'tailwind-merge';
 
-const ProgressBar = ({ percentage, className }: { percentage: number; className?: string }) => {
+const ProgressBar = ({ percentage: initialPercentage, className }: { percentage: number; className?: string }) => {
   const progressBarClass = twMerge(clsx('flex items-center gap-2  py-1 w-full bg-white rounded-full', className));
+  const [percentage, setPercentage] = useState(0);
+
+  useEffect(() => {
+    const timer = setTimeout(() => {
+      setPercentage(initialPercentage);
+    }, 300);
+    return () => clearTimeout(timer);
+  }, [initialPercentage]);
+
   return (
     <div className={progressBarClass}>
       <div className='relative w-full bg-slate-100 rounded-full h-1'>
