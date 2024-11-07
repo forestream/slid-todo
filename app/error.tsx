3cb2c985c5721aa2a8a81a018c83e727f97a0ebe
mokoko <guswlso2@naver.com>
2024-11-07 09:46:45 +0900

    🐛 #232 fix: 404 페이지 버튼 수정

--- app/error.tsx
+++ app/error.tsx
@@ -2,9 +2,17 @@
 
 import Button from '@/components/common/ButtonSlid';
 import MainLogo from '@/public/images/MainLogo';
-import Link from 'next/link';
+import { useRouter } from 'next/navigation';
 
 export default function NotFound() {
+  const router = useRouter();
+
+  const handleGoHome = () => {
+    router.push('/');
+  };
+  const handleGoBack = () => {
+    router.back();
+  };
   return (
     <div className='min-h-screen bg-gradient-to-b from-blue-100 to-purple-100 flex flex-col items-center justify-center p-4'>
       <MainLogo className={`mb-[60px]`} />
@@ -19,11 +27,11 @@ export default function NotFound() {
         <h2 className='text-2xl font-semibold text-gray-800 mb-4'>앗! 페이지를 찾을 수 없어요</h2>
         <p className='text-gray-600 mb-8'>찾으시는 페이지가 사라졌거나 잘못된 주소를 입력하셨어요.</p>
         <div className='flex flex-col sm:flex-row justify-center gap-4'>
-          <Button onClick={() => window.history.back()} className='flex items-center justify-center'>
+          <Button onClick={handleGoBack} className='flex items-center justify-center'>
             뒤로 가기
           </Button>
-          <Button className='flex items-center justify-center' variant='outlined'>
-            <Link href='/'>홈으로 가기</Link>
+          <Button onClick={handleGoHome} className='flex items-center justify-center' variant='outlined'>
+            홈으로 가기
           </Button>
         </div>
       </div>
