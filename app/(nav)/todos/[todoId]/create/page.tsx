ee02ca933bb12845d0283de175ec1e63b86da0c4
hanbin <rj2303@naver.com>
2024-11-11 16:18:05 +0900

    💄 #265 style: 노스 생성/수정 페이지 스크롤 적용

--- app/(nav)/todos/[todoId]/create/page.tsx
+++ app/(nav)/todos/[todoId]/create/page.tsx
@@ -2,7 +2,7 @@ import NoteFormSections from '../_view/NoteFormSections';
 
 export default function Page() {
   return (
-    <main className='lg:flex h-screen'>
+    <main className='lg:flex h-screen overflow-auto'>
       <NoteFormSections />
     </main>
   );
