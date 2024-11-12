d5d628711d7b65a576b29cb238e84bd1a3d5019d
mokoko <guswlso2@naver.com>
2024-11-12 11:22:59 +0900

    ✏️#273 fix: '인베드'를 '임베드'로 수정하여 문구 일관성 개선

--- components/landingPage/FourthSection.tsx
+++ components/landingPage/FourthSection.tsx
@@ -29,10 +29,10 @@ const FourthSection = ({ isVisible }: SectionProps) => {
         className='w-full h-full flex flex-col justify-center'
       >
         <motion.h2 variants={fadeInUp} className='text-4xl font-bold'>
-          노트 작성과 인베드 기능
+          노트 작성과 임베드 기능
         </motion.h2>
         <motion.p variants={fadeInUp} className='mt-4 lg:text-xl'>
-          할 일마다 1:1로 연결된 노트에 더 자세한 내용을 기록할 수 있습니다. 유연한 인베드 기능으로 참고할 외부 자료나
+          할 일마다 1:1로 연결된 노트에 더 자세한 내용을 기록할 수 있습니다. 유연한 임베드 기능으로 참고할 외부 자료나
           링크를 편하게 참조해 작업 효율성을 높여보세요.
         </motion.p>
       </motion.div>
