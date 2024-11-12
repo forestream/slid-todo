74588a94a2f0c3bdf5829815eacfeb0bf493a3c5
MinaRoh <00minha@gmail.com>
2024-11-12 18:53:36 +0900

    ✨ #275 feat: nav의 목표 hover시 kebab 아이콘으로 수정/삭제 기능 추가
    - 목표에 hover시 kebab 아이콘이 보이도록 추가했습니다.
    - 목표간 간격이 좁아 kebab 아이콘 클릭시 하단에 등장하는 DropdownMenu와 kebab 아이콘 사이의 간격을 최소화했습니다.
    - nav 목표의 글자크기에 맞게 DropdownMenu의 글자 크기도 조절했습니다.
    - DropdownMenu의 수정하기 클릭시 SlidInput 태그로 변경되며, 해당 목표만 수정 가능하고, 엔터로 제출됩니다. 포커스 해제시 기존처럼 목표 link 태그 상태로 변경됩니다.
    - DropdownMenu의 삭제하기 클릭시 ConfirmationModal에 목표 텍스트가 전달되며 '{itemTitle} 목표를 삭제하시겠습니까?' 메세지로 현재 삭제하려는 목표를 명확하게 표현하도록 변경했습니다.

--- components/nav/NavGoal.tsx
+++ components/nav/NavGoal.tsx
@@ -10,12 +10,24 @@ import InputSlid from '../common/InputSlid';
 import AddGoalButton from './AddGoalButton';
 import clsx from 'clsx';
 import { twMerge } from 'tailwind-merge';
+import DropdownMenu from '../common/DropdownMenu';
+import { IconKebabWithCircle } from '@/public/icons/IconKebabWithCircle';
+import { useDeleteGoalMutation } from '@/lib/hooks/useDeleteGoalMutation';
+import { useUpdateGoalMutation } from '@/lib/hooks/useUpdateGoalMutation';
+import ConfirmationModal from '../modal/ConfirmationModal';
 
 const DEFAULT_INPUT_VALUE = '· ';
 const NavGoal = ({ className }: { className?: string }) => {
   const [isGoalInputVisible, setIsGoalInputVisible] = useState(false);
   const [goalInputValue, setGoalInputValue] = useState('');
   const [selectedGoalId, setSelectedGoalId] = useState<number | null>(null);
+  const [kebabClickedGoal, setKebabClickedGoal] = useState<Goal>();
+  const deleteGoal = useDeleteGoalMutation();
+  const updateGoal = useUpdateGoalMutation();
+
+  const [isEditFocused, setIsEditFocused] = useState(false);
+  const [editingGoalId, setEditingGoalId] = useState(-1);
+  const [isDeleteModalOpen, setIsDeleteModalOpen] = useState(false);
 
   const addGoal = useAddGoalMutation();
 
@@ -60,58 +72,140 @@ const NavGoal = ({ className }: { className?: string }) => {
     setGoalInputValue('· ');
   };
 
+  const handleDropdownMenuClick = (item: string) => {
+    if (item === '수정하기') {
+      setIsEditFocused(true);
+    } else if (item === '삭제하기') {
+      setIsDeleteModalOpen(true);
+    }
+  };
+
+  const handleNavGoalKebabClick = (goal: Goal) => {
+    setKebabClickedGoal(goal);
+    setEditingGoalId(goal.id);
+  };
+
+  const handleGoalInputChange = (e: React.ChangeEvent<HTMLInputElement>) => {
+    setGoalInputValue(e.target.value);
+  };
+
+  // 수정
+  const handleGoalEditSubmit = (e?: React.FormEvent) => {
+    e?.preventDefault();
+    updateGoal.mutate(
+      { id: kebabClickedGoal?.id ?? -1, updates: { title: goalInputValue.trim() } },
+      {
+        onSuccess: (data) => {
+          setGoalInputValue(data.title);
+          setIsEditFocused(false);
+        },
+      }
+    );
+  };
+
+  // 삭제
+  const handleGoalDelete = () => {
+    deleteGoal.mutate({ goalId: kebabClickedGoal?.id ?? -1 });
+    setIsDeleteModalOpen(false);
+  };
+
   return (
-    <div className={twMerge(clsx('flex flex-wrap px-4 py-6 gap-4', className))}>
-      <div className='flex items-center gap-2 order-1 sm:order-1 lg:order-1'>
-        <div className='w-6 h-6 flex justify-center items-center'>
-          <IconFlagSmall />
-        </div>
-        <div className='text-lg font-medium text-slate-800'>목표</div>
-      </div>
-
-      {/* 새 목표 버튼 (모바일에서는 타이틀 옆, 태블릿과 데스크탑에서는 맨 아래로) */}
-      <AddGoalButton
-        className='order-2 sm:order-4 lg:order-4 ml-auto sm:mx-0 lg:mx-0 gap-[2px] rounded-xl text-sm px-3 py-2 sm:p-3 lg:p-3 sm:px-6 lg:px-6 mt-0 w-[84px] sm:w-full lg:w-full'
-        onClick={handleAddGoalButtonClick}
-      />
-
-      <div className='order-3 sm:order-2 lg:order-2 w-full max-h-72 overflow-y-auto h-auto'>
-        {data?.pages.map((page, idx) => (
-          <div key={page.nextCursor || idx} className='flex-col'>
-            {page.goals.map((goal: Goal) => (
-              <Link
-                href={`/goals/${goal.id}`}
-                key={goal.id}
-                className={clsx(
-                  'flex text-slate-700 text-sm font-medium rounded-lg p-2 hover:bg-slate-50',
-                  goal.id === selectedGoalId ? 'bg-slate-50' : 'bg-white'
-                )}
-                onClick={() => handleGoalClick(goal.id)}
-              >
-                {`${DEFAULT_INPUT_VALUE}${goal.title}`}
-              </Link>
-            ))}
+    <>
+      <div className={twMerge(clsx('flex flex-wrap px-4 py-6 gap-4', className))}>
+        <div className='flex items-center gap-2 order-1 sm:order-1 lg:order-1'>
+          <div className='w-6 h-6 flex justify-center items-center'>
+            <IconFlagSmall />
           </div>
-        ))}
-        <div ref={ref}></div>
-      </div>
-
-      {/* 새 목표 클릭시 생성되는 인풋 */}
-      {isGoalInputVisible && (
-        <div className='order-4 sm:order-3 lg:order-3 w-full flex items-center'>
-          <form onSubmit={handleGoalSubmit} className='w-full h-auto m-0'>
-            <InputSlid
-              type='text'
-              className='m-0'
-              inputClassName='flex-grow p-2 m-0 text-sm font-medium bg-white rounded-lg border-2 border-blue-200'
-              value={goalInputValue}
-              onChange={handleInputChange}
-              autoFocus
-            />
-          </form>
+          <div className='text-lg font-medium text-slate-800'>목표</div>
         </div>
-      )}
-    </div>
+
+        {/* 새 목표 버튼 (모바일에서는 타이틀 옆, 태블릿과 데스크탑에서는 맨 아래로) */}
+        <AddGoalButton
+          className='order-2 sm:order-4 lg:order-4 ml-auto sm:mx-0 lg:mx-0 gap-[2px] rounded-xl text-sm px-3 py-2 sm:p-3 lg:p-3 sm:px-6 lg:px-6 mt-0 w-[84px] sm:w-full lg:w-full'
+          onClick={handleAddGoalButtonClick}
+        />
+
+        <div className='order-3 sm:order-2 lg:order-2 w-full max-h-72 overflow-y-auto h-auto'>
+          {data?.pages.map((page, idx) => (
+            <div key={page.nextCursor || idx} className='flex-col'>
+              {page.goals.map((goal: Goal) => (
+                <div className='flex items-center group rounded-lg  hover:bg-slate-50 ' key={goal.id}>
+                  {/* kebab에서 수정하기를 클릭하면 Input, 그 외에는 일반 Link로 goal list */}
+                  {isEditFocused && editingGoalId === goal.id ? (
+                    <form onSubmit={handleGoalEditSubmit} className='w-full h-auto m-0'>
+                      <InputSlid
+                        type='text'
+                        placeholder={goal?.title}
+                        className='my-0 mx-2'
+                        inputClassName='flex-grow py-1 px-2 text-xs font-medium rounded-lg'
+                        value={goalInputValue}
+                        onChange={handleGoalInputChange}
+                        autoFocus
+                        onBlur={(e) => {
+                          if (!e.currentTarget.contains(e.relatedTarget)) {
+                            setIsEditFocused(false);
+                          }
+                        }}
+                      />
+                    </form>
+                  ) : (
+                    <>
+                      <Link
+                        href={`/goals/${goal.id}`}
+                        key={goal.id}
+                        className={clsx(
+                          'flex flex-grow text-slate-700 text-sm font-medium group-hover:bg-slate-50 p-2 hover-trigger',
+                          goal.id === selectedGoalId ? 'bg-slate-50' : 'bg-white'
+                        )}
+                        onClick={() => handleGoalClick(goal.id)}
+                      >
+                        {`${DEFAULT_INPUT_VALUE}${goal.title}`}
+                      </Link>
+                      <div
+                        className='items-center justify-center ml-auto hidden group-hover:flex'
+                        onClick={() => handleNavGoalKebabClick(goal)}
+                      >
+                        <DropdownMenu
+                          icon={IconKebabWithCircle}
+                          dropdownList={['수정하기', '삭제하기']}
+                          onItemClick={handleDropdownMenuClick}
+                          className='space-y-0'
+                          dropdownListClassName='text-sm'
+                        />
+                      </div>
+                    </>
+                  )}
+                </div>
+              ))}
+            </div>
+          ))}
+          <div ref={ref}></div>
+        </div>
+
+        {/* 새 목표 클릭시 생성되는 인풋 */}
+        {isGoalInputVisible && (
+          <div className='order-4 sm:order-3 lg:order-3 w-full flex items-center'>
+            <form onSubmit={handleGoalSubmit} className='w-full h-auto m-0'>
+              <InputSlid
+                type='text'
+                className='m-0'
+                inputClassName='flex-grow p-2 m-0 text-sm font-medium bg-white rounded-lg border-2 border-blue-200'
+                value={goalInputValue}
+                onChange={handleInputChange}
+                autoFocus
+              />
+            </form>
+          </div>
+        )}
+      </div>
+      <ConfirmationModal
+        isOpen={isDeleteModalOpen}
+        onChangeIsOpen={setIsDeleteModalOpen}
+        itemType='goal'
+        itemTitle={kebabClickedGoal?.title}
+        onConfirm={handleGoalDelete}
+      />
+    </>
   );
 };
 
