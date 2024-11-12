2cdd6c16ef65f5becebc81e7c27d23217b41d0d9
mokoko <guswlso2@naver.com>
2024-11-12 15:32:55 +0900

    ✨#281 feat: 로딩 컴포넌트 추가 및 애니메이션 설정

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -25,11 +25,16 @@ const config: Config = {
           '0%': { transform: 'translateX(100%)', opacity: '0' },
           '100%': { transform: 'translateX(0)', opacity: '1' },
         },
+        loading: {
+          '0%': { transform: 'translateX(-100%)' },
+          '100%': { transform: 'translateX(100%)' },
+        },
       },
       animation: {
         shake: 'shake 0.5s ease-in-out',
         'slide-up': 'slide-up 0.5s ease-out',
         'slide-in-right': 'slide-in-right 0.5s ease-out',
+        'loading-bar': 'loading 1.5s ease-in-out infinite',
       },
     },
   },
