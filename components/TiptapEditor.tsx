4154ce60cd7c2297292402cc0de184025e61d89a
hanbin <rj2303@naver.com>
2024-11-14 10:46:09 +0900

    ✨ #309 feat: 노트작성페이지 텍스트에디터 포커스 영역 확장

--- components/TiptapEditor.tsx
+++ components/TiptapEditor.tsx
@@ -6,14 +6,14 @@ import { PropsWithChildren } from 'react';
 const TiptapEditor = ({ ...props }: PropsWithChildren) => {
   const { editor } = useCurrentEditor();
 
-  if (!editor) return;
+  const handleClick = () => editor?.chain().focus().run();
 
   return (
-    <div className='grow lg:relative'>
+    <div className='grow lg:relative cursor-text' onClick={handleClick}>
       <div className='overflow-visible h-full'>
         <EditorContent editor={editor} {...props} />
         <div className='h-20'></div>
-        <input type='hidden' name='contentHTML' value={editor.getHTML()} />
+        <input type='hidden' name='contentHTML' value={editor?.getHTML()} />
       </div>
     </div>
   );
