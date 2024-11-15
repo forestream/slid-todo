66aca617f42b28df13f9d8ba65f086e22b01e1e9
mokoko <guswlso2@naver.com>
2024-11-15 17:07:00 +0900

    🐛#340 fix: Alltodos 함수에 검색 매개변수 추가, TodoImage 컴포넌트에 unoptimized 속성 추가, TodoItem 레이아웃 수정

--- components/common/todoItem/index.tsx
+++ components/common/todoItem/index.tsx
@@ -34,12 +34,14 @@ const areEqual = (prevProps: TodoItemProps, nextProps: TodoItemProps) => {
 const TodoItem: React.FC<TodoItemProps> = memo(({ data, viewGoal, variant = 'default' }) => {
   const layoutClasses = {
     article: twMerge(
-      'text-sm grid gap-x-2 items-center',
-      variant === 'default' ? 'grid-cols-[auto_1fr_auto]' : 'grid-cols-[auto_1fr_auto_auto]'
+      'text-sm grid gap-x-2 items-start', // items-center를 items-start로 변경
+      variant === 'default' ? 'grid-cols-[auto_minmax(0,1fr)_auto]' : 'grid-cols-[auto_minmax(0,1fr)_auto_auto]'
+      // minmax(0,1fr)을 사용하여 자식 요소가 부모 컨테이너를 넘어가지 않도록 함
     ),
+    content: 'min-w-0 flex flex-col gap-1', // 내용을 감싸는 컨테이너 추가
     checkIcon: 'row-start-1 col-start-1 h-6',
-    title: twMerge('row-start-1 col-start-2 line-clamp-1'),
-    goal: twMerge('row-start-2 col-start-2', variant === 'detailed' && 'row-start-2 col-start-2'),
+    title: 'min-w-0 line-clamp-1', // min-w-0 추가
+    goal: 'min-w-0', // min-w-0 추가
     todoIcon: twMerge('row-start-1 col-start-3', variant === 'detailed' && 'col-start-4'),
     image: 'row-start-3 col-span-full w-full mt-2',
   };
@@ -49,14 +51,16 @@ const TodoItem: React.FC<TodoItemProps> = memo(({ data, viewGoal, variant = 'def
       <div className={layoutClasses.checkIcon}>
         <CheckIcon done={data.done} id={data.id} />
       </div>
-      <div className={layoutClasses.title}>
-        <TodoTitle data={data} variant={variant} />
-      </div>
-      {viewGoal && data.goal?.id && (
-        <div className={layoutClasses.goal}>
-          <GoalTitle goal={data.goal} />
+      <div className={layoutClasses.content}>
+        <div className={layoutClasses.title}>
+          <TodoTitle data={data} variant={variant} />
         </div>
-      )}
+        {viewGoal && data.goal?.id && (
+          <div className={layoutClasses.goal}>
+            <GoalTitle goal={data.goal} />
+          </div>
+        )}
+      </div>
       <div className={layoutClasses.todoIcon}>
         <TodoIcon data={data} />
       </div>
