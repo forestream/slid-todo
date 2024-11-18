1f5d80dba33b7d84246431e3c2b54993070596d1
mokoko <guswlso2@naver.com>
2024-11-18 11:05:47 +0900

    💄#344 style: NavMobileSheet 컴포넌트의 SheetContent에 패딩 제거

--- components/nav/NavMobileSheet.tsx
+++ components/nav/NavMobileSheet.tsx
@@ -11,7 +11,7 @@ interface NavMobileSheetProps {
 const NavMobileSheet: React.FC<NavMobileSheetProps> = ({ isSheetOpen, setIsSheetOpen, handleTodoModalOpen }) => {
   return (
     <SheetProvider isOpen={isSheetOpen} onChangeIsOpen={setIsSheetOpen}>
-      <SheetContent position={'top'} dialogClassName={'sm:hidden'}>
+      <SheetContent position={'top'} dialogClassName={'sm:hidden'} className='p-0'>
         <div className='flex items-center justify-between pt-3 px-4 pb-4'>
           <ImageLogoWithText />
           <SheetClose />
