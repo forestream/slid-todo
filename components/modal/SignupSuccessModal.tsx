e79050ec97853d05583e50bcf67779773d4d3f3d
mokoko <guswlso2@naver.com>
2024-11-21 10:14:24 +0900

    ✨#331 feat: 회원가입 성공 모달에 ConfettiCanvas 추가 및 이미지로 변경

--- components/modal/SignupSuccessModal.tsx
+++ components/modal/SignupSuccessModal.tsx
@@ -1,6 +1,12 @@
 import { ModalContent, ModalProvider } from '../common/Modal';
 import Button from '../common/ButtonSlid';
 import { useRouter } from 'next/navigation';
+import Image from 'next/image';
+import dynamic from 'next/dynamic';
+
+const ConfettiCanvas = dynamic(() => import('../ConfettiCanvas'), {
+  ssr: false,
+});
 
 interface SignupSuccessModalProps {
   isOpen: boolean;
@@ -13,16 +19,21 @@ const SignupSuccessModal: React.FC<SignupSuccessModalProps> = ({ isOpen, onChang
     router.push('/login');
   };
   return (
-    <ModalProvider isOpen={isOpen} onChangeIsOpen={onChangeIsOpen}>
-      <ModalContent closeOnClickOverlay={false}>
-        <div className='flex flex-col items-center gap-4'>
-          <h1 className='text-lg font-bold'>회원가입 성공</h1>
-          <div className='flex justify-center items-center w-60 h-60'>뭔가 엄청나게 축하하는 이미지</div>
-          <p className='text-sm text-center'>회원가입이 성공적으로 완료되었습니다. 로그인 후 서비스를 이용해주세요.</p>
-          <Button onClick={handleRedirect}>확인</Button>
-        </div>
-      </ModalContent>
-    </ModalProvider>
+    <>
+      <ModalProvider isOpen={isOpen} onChangeIsOpen={onChangeIsOpen}>
+        <ModalContent closeOnClickOverlay={false}>
+          <div className='flex flex-col items-center gap-4'>
+            <h1 className='text-lg font-bold'>회원가입 성공</h1>
+            <Image src='/images/ImageSina.svg' width={400} height={400} alt='회원가입 성공' />
+            <p className='text-sm text-center'>
+              회원가입이 성공적으로 완료되었습니다. 로그인 후 서비스를 이용해주세요.
+            </p>
+            <Button onClick={handleRedirect}>확인</Button>
+          </div>
+        </ModalContent>
+      </ModalProvider>
+      <ConfettiCanvas isActive={isOpen} />
+    </>
   );
 };
 
